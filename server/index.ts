5979734bdba7760c287adcbbb263124d057d4bf9
srinu6663 <srinuyejarla874@gmail.com>
2025-08-29 21:43:20 +0530

    server correcting

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-const host = process.env.HOST || "127.0.0.1"; // fallback to localhost
+const host = process.env.HOST || "0.0.0.0"; // bind to all interfaces for cloud deployment
 
   server.listen(
     {
